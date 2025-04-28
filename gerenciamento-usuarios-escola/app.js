7f789968775dd0f800ea38084dc09c947045365b
Nicollas R Deluca M <nicollas83@gmail.com>
2025-04-28 16:00:51 -0300

    att 4 - angularjs - incopmp

---
+++ gerenciamento-usuarios-escola/app.js
@@ -0,0 +1,9 @@
+angular.module('appEscola', [])
+.controller('AppController', function($scope) {
+  $scope.mensagem = "Bem-vindo ao sistema de cadastro escolar";
+
+  $scope.usuario = {
+    nome: "João",
+    tipo: "Aluno"
+  };
+});
